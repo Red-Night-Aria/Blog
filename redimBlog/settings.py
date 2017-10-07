f18c254ed3a82adcd4a36da9761c6af3e5640993
Redim <Red.Night.Aria@gmail.com>
2017-10-07 21:38:30 +0800

    add comment plug-in

--- redimBlog/settings.py
+++ redimBlog/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qa1-v!xtfo7lvt1_qsc(7bvmjnj)3u-!@)k=)#=761ytmz75g2'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['redim.space']
 
 
 # Application definition
